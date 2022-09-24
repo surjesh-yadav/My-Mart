3261f2cd5a5af92ea12b984706863d1a3727fd2a
Surjesh Yadav <97423078+surjesh-yadav@users.noreply.github.com>
2022-09-24 10:27:50 +0530

    Update actions.js

--- src/Redux/auth/actions.js
+++ src/Redux/auth/actions.js
@@ -2,7 +2,6 @@ import { Axios } from "axios";
 export const SIGNIN_REQUEST = "SIGNIN_REQUEST";
 export const SIGNIN_SUCCESS = "SIGNIN_SUCCESS";
 export const SIGNIN_FAILURE = "SIGNIN_FAILURE";
-
 const signInRequest = () => {
   return {
     type: SIGNIN_REQUEST,
